291e408acb7f0af66604e9d0652a14e5e5f1c42a
Ceglédi Iván <cegledi.ivan74@gmail.com>
2020-05-20 08:47:13 +0200

    kategórizáló admn javítás3

--- application/helpers/main_helper.php
+++ application/helpers/main_helper.php
@@ -140,21 +140,27 @@ function ws_wrap($str, $max=100) {
 }
 
 // url kompatibilis str nem utf szövegekből
+function normalizeStr($str) {
+    $table = array(
+        'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+        'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+        'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+        'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+        'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+        'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+        'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+        'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r',
+    );
+   
+    return strtr($str, $table);
+	
+}
 function strToUrl($str) {
 	
-	$EXCEPT_CHAR = "ÁáÀàĂăẮắẰằẴẵẲẳÂâẤấẦầẪẫẨẩǍǎÅåǺǻÄäǞǟÃãȦȧǠǡĄąĀāẢảȀȁȂȃẠạẶặẬậḀḁȺⱫᶏḂḃḄḅḆḇɃƀᵬᶀƁɓƂƃĆćĈĉČčĊċÇçḈḉȻȼƇƈɕĎďḊḋḐḑḌḍḒḓḎḏĐđᵭᶁƉɖƊɗᶑƋƌȡÉéÈèĔĕÊêẾếỀềỄễỂểĚěËëẼẽĖėȨȩḜḝĘęĒēḖḗḔḕẺẻȄȅȆȇẸẹỆệḘḙḚḛɆɇᶒḞḟᵮᶂƑƒǴǵĞğĜĝǦǧĠġĢģḠḡǤǥᶃƓɠĤĥȞȟḦḧḢḣḨḩḤḥḪḫHẖĦħⱧⱨÍíÌìĬĭÎîǏǐÏïḮḯĨĩİiĮįĪīỈỉȈȉȊȋỊịḬḭIıƗɨᵻᶖĴĵǰȷɈɉʝɟʄḰḱǨǩĶķḲḳḴḵᶄƘƙⱩⱪĹĺĽľĻļḶḷḸḹḼḽḺḻŁłĿŀȽƚⱠⱡⱢɫɬᶅɭȴḾḿṀṁṂṃᵯᶆɱŃńǸǹŇňÑñṄṅŅņṆṇṊṋṈṉᵰƝɲȠƞᶇɳȵNnÓóÒòŎŏÔôỐốỒồỖỗỔổǑǒÖöȪȫŐőÕõṌṍṎṏȬȭȮȯȰȱØøǾǿǪǫǬǭŌōṒṓṐṑỎỏȌȍȎȏƠơỚớỜờỠỡỞởỢợỌọỘộᶗƟɵṔṕṖṗⱣᵽᵱᶈƤƥPpʠɊɋŔŕŘřṘṙŖŗȐȑȒȓṚṛṜṝṞṟɌɍᵲᶉɼⱤɽɾᵳŚśṤṥŜŝŠšṦṧṠṡẛŞşṢṣṨṩȘșᵴᶊʂȿSsŤťTẗṪṫŢţṬṭȚțṰṱṮṯŦŧȾⱬᵵƫƬƭƮʈȶÚúÙùŬŭÛûǓǔŮůÜüǗǘǛǜǙǚǕǖŰűŨũṸṹŲųŪūṺṻỦủȔȕȖȗƯưỨứỪừỮữỬửỰựỤụṲṳṶṷṴṵɄʉᵾᶙṼṽṾṿᶌƲʋⱴẂẃẀẁŴŵWẘẄẅẆẇẈẉẌẍẊẋᶍÝýỲỳŶŷYẙŸÿỸỹẎẏȲȳỶỷỴỵʏɎɏƳƴŹźẐẑŽžŻżẒẓẔẕƵƶᵶᶎȤȥʐʑɀⱫⱬɐɑᶐɒẚÆæǼǽǢǣʙↃↄÐðȸʣʥʤƎǝƏəᶕƐɛᶓɘɚɜᶔɝɞʚɤʩℲⅎɡᵹɢʛᵷƔɣƢƣʜǶƕɦⱵⱶɧɪƖɩᵼʞʪʫʟɮƛʎɴŊŋŒœɶƆɔɷȢȣɸⱷȹKʻĸƦʀɹɺɻɿʁßƩʃᶋƪʅᶘʆſʨᵺƾʦʧʇᵫɄʉɥʮʯƜɯɰƱʊᵿɅʌʍƍƷʒǮǯƸƹᶚƺʓȜȝÞþǷƿƻƧƨƼƽƄƅɁɂʔʕʡʢʖǀǁǂǃʗʘʬʭЗзЧч";
-	$EXCEPT_CHAR_REPLACE = "AaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAzaBbBbBbBbbbBbBbCcCcCcCcCcCcCcCccDdDdDdDdDdDdDdddDdDdddddEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeeFfffFfGgGgGgGgGgGgGgGggGgHhHhHhHhHhHhHhHhHhHhIiIiIiIiIiIiIiIiIiIiIiIiIiIiIiIiIiIiIiJjJjJjjjjKkKkKkKkKkKkKkkLlLlLlLlLlLlLlLlLlLlLlLlLlLlMmMmMmMmmNnNnNnNnNnNnNnNnNnnNnNnNnnNnOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOocdePpPpPpppPpPpqQqRrRrRrRrRrRrRrRrRrRrrrrRrrrSsSsSsSsSsSsFSsSsSsSsSsssSsTtTtTtTtTtTtTtTtTtTzttTtTttUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuUuuuVvVvvVvvWwWwWwWwWwWwWwXxXxxYyYyYyYyYyYyYyYyYyYyyyyYyZzZzZzZzZzZzZzzzZzZzzZzaaqoaAaAaAaBCcDdddddEeEeaEeeeeeeegdyfFfgggggYyMmhHhhtthillzkkklBAAnNnEeeCcobbowpK'kRrjJyrbBEfLllllftwsttteVvyYYWwwUuuAamGEEEEEeeeeEEPPPpZZzbbbbCcCCCCClltlcOWMBbHh";
-	
-	$plattern = array();
-	for($i=0;$i<mb_strlen($EXCEPT_CHAR, 'UTF-8');$i++) $plattern[]=$EXCEPT_CHAR[$i];
-
-	$replacement = array();
-	for($i=0;$i<mb_strlen($EXCEPT_CHAR, 'UTF-8');$i++) $plattern[]=$EXCEPT_CHAR[$i];
-
+	$str= normalizeStr($str);
 	while(strstr($str, "..")) $str = str_replace("..", ".", $str);
 
-	$str = SubStr(str_replace($plattern, $replacement, $str), 0, 1000);
-
+	
 	$str = StrToLower(preg_replace("/\W/", "-", $str));
 
 	while(strstr($str, "--")) $str = str_replace("--", "-", $str);
